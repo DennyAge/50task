14dd356e63be98e8b27e702cad4f0febea918540
Denys <DenX055@gmail.com>
2021-08-11 17:39:42 +0300

    Add Split Landing Page

--- Split Landing Page/script.js
+++ Split Landing Page/script.js
@@ -1,8 +1,10 @@
-const search = document.querySelector('.search')
-const btn = document.querySelector('.btn')
-const input = document.querySelector('.input')
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
 
-btn.addEventListener('click', () => {
-  search.classList.toggle('active')
-  input.focus()
-})
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+
